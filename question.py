fe8594d09ac601bc7c2cfcb5b7c06bfa37b29159
minister 2020 <abdulrasheedidowu86@gmail.com>
2022-06-23 10:59:33 +0100

    third commit

--- question.py
+++ question.py
@@ -3,18 +3,29 @@ from questions import question
 
 
 question_prompt =[
-    "what colour are apples? \n(a) Red \n (b) Purple\n (c) Yello\n\n",
-    "what colour are Banana/ \n (a) Green \n (b) White \n (c) Blue\n\n",
-    "what colur are Mango? \n (a) Green \n (b) Yello \n (c) Pink\n\n",
-    "what colur are watermelon? \n (a) White \n(b) Blue \n (c) Green",
-    "what  "
+    "1. what colour are apples? \n(a) Red \n (b) Purple\n (c) Yello\n\n",
+    "2. what colour are Banana/ \n (a) Green \n (b) White \n (c) Blue\n\n",
+    "3. what colur are Mango? \n (a) Green \n (b) Yello \n (c) Pink\n\n",
+    "4. what colur are watermelon? \n (a) White \n(b) Blue \n (c) Green\n\n",
+    "5. who is the head of the family? \n (a) father \n(b) mother \n (c) sister\n\n",
+    "6. who is the Governor of lagos state? \n(a) Tinube\n (b)Fashola \n (c) Sanwo-olu\n\n"
+    "7. who is the Governor of Oyo state? \n(a) Makinde \n (b)Abasha \n (c) Buhari\n\n",
+    "8. when did Nigeria got her independent?\n (a)1990\n (b)2004 \n (c)1960\n\n",
+    "9. a  is greather than b? \n (a) yes\n (b)No\n\n"
+
 
 ]
 
 questions = [
     question(question_prompt[0], "a"),
     question(question_prompt[1], "a"),
-    question(question_prompt[2], "b")
+    question(question_prompt[2], "b"),
+    question(question_prompt[3], "c"),
+    question(question_prompt[4], "a"),
+    question(question_prompt[5], "c"),
+    question(question_prompt[6], "a"),
+    question(question_prompt[7], "c"),
+  
 ]
 
 
